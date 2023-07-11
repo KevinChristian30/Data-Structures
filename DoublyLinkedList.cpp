7d4f6d15a050c56f845a5dcbf690cc73ea094f10
Kevin Christian <kdotchrist30@gmail.com>
2023-07-11 23:16:26 +0700

    Added Doubly Linked List

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -5,35 +5,39 @@ struct Node {
   int data;
 
   Node *next;
+  Node *prev;
 };
 
 Node *Head = NULL;
-Node *Tail = NULL;
+Node *Tail= NULL;
 
 Node *createNode(Node node) {
-  Node *newNode = (Node *) malloc(sizeof(Node));
+  Node *newNode = (Node*) malloc(sizeof(Node));
+  
   newNode->data = node.data;
-  newNode->next = NULL;
+  newNode->next = newNode->prev = NULL;
   
   return newNode;
 }
 
 void pushHead(Node toInsert) {
   Node *newNode = createNode(toInsert);
-  
+
   if (!Head) Head = Tail = newNode;
   else {
     newNode->next = Head;
+    Head->prev = newNode;
     Head = newNode;
   }
 }
 
 void pushTail(Node toInsert) {
   Node *newNode = createNode(toInsert);
-
+  
   if (!Head) Head = Tail = newNode;
   else {
     Tail->next = newNode;
+    newNode->prev = Tail;
     Tail = newNode;
   }
 }
@@ -43,12 +47,13 @@ void pushPriority(Node toInsert) {
   else if (toInsert.data > Tail->data) pushTail(toInsert);
   else {
     Node *newNode = createNode(toInsert), *current;
-
     for (current = Head; current; current = current->next) 
-      if (newNode->data < current->next->data) break;
-    
-    newNode->next = current->next;
-    current->next = newNode;
+      if (newNode->data < current->data) break;
+
+    newNode->next = current;
+    newNode->prev = current->prev;
+    current->prev->next = newNode;
+    current->prev = newNode;
   }
 }
 
@@ -82,26 +87,22 @@ void popHead() {
     return;
   }
 
-  Node *toDeallocate = Head;
   Head = Head->next;
-  free(toDeallocate);
-  toDeallocate = NULL;
+  free(Head->prev);
+  Head->prev = NULL;
 }
 
 void popTail() {
   if (!Head) return;
 
-  if (Head == Tail) {
+  if (Head == Tail){
     free(Head);
     Head = Tail = NULL;
 
     return;
   }
 
-  Node *behindTail;
-  for (behindTail = Head; behindTail->next != Tail; behindTail = behindTail->next);
-
-  Tail = behindTail;
+  Tail = Tail->prev;
   free(Tail->next);
   Tail->next = NULL;
 }
@@ -109,22 +110,29 @@ void popTail() {
 void remove(int toRemove) {
   if (!Head) return;
 
+  if (Head == Tail){
+    free(Head);
+    Head = Tail = NULL;
+
+    return;
+  }
+
   if (Head->data == toRemove) popHead();
   else if (Tail->data == toRemove) popTail();
   else {
-    Node *previousToRemove;
-    for (previousToRemove = Head; previousToRemove; previousToRemove = previousToRemove->next)
-      if (previousToRemove->next->data == toRemove) break;
+    Node *current;
+    for (current = Head->next; current; current = current->next)
+      if (current->data == toRemove) break;
 
-    if (!previousToRemove) return;
+    if (!current) return;
 
-    Node *toDeallocate = previousToRemove->next;
-    previousToRemove->next = previousToRemove->next->next;
-    free(toDeallocate);
-    toDeallocate = NULL;
+    current->prev->next = current->next;
+    current->next->prev = current->prev;
+    free(current);
+    current = NULL;
   }
 }
-
-int main() {
+ 
+int main() {  
   return 0;
 }
